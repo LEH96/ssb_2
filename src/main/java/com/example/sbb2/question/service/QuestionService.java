7c03e624d512c54c12307888396bc3ee0e45cecb
LEH96 <lutea67@naver.com>
2023-03-26 00:43:29 +0900

    검색기능 추가

--- src/main/java/com/example/sbb2/question/service/QuestionService.java
+++ src/main/java/com/example/sbb2/question/service/QuestionService.java
@@ -1,14 +1,17 @@
 package com.example.sbb2.question.service;
 
 import com.example.sbb2.DataNotFoundException;
+import com.example.sbb2.answer.entity.Answer;
 import com.example.sbb2.question.entity.Question;
 import com.example.sbb2.question.repository.QuestionRepository;
 import com.example.sbb2.user.entity.SiteUser;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -20,6 +23,27 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class QuestionService {
     private final QuestionRepository questionRepository;
+
+    private Specification<Question> search(String kw) {
+        return new Specification<Question>() {
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true); //중복 제거
+                //question 테이블과 user 테이블을 question_author_id 기준으로 left join
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                //question 테이블과 answer 테이블을 question_id 기준으로 left join
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                //answer 테이블과 user 테이블을 answer_author_id 기준으로 left join
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), //제목 조회
+                        cb.like(q.get("content"), "%" + kw + "%"), //질문 내용 조회
+                        cb.like(u1.get("username"), "%" + kw + "%"), //질문 작성자 조회
+                        cb.like(a.get("content"), "%" + kw + "%"), //답변 내용 조회
+                        cb.like(u2.get("username"), "%" + kw + "%")); //답변 작성자 조회
+            }
+        };
+    }
+
     public Question write(String subject, String content, SiteUser user) {
         Question q = new Question();
         q.setSubject(subject);
@@ -66,5 +90,12 @@ public class QuestionService {
     public void vote(Question question, SiteUser siteUser) {
         question.getVoter().add(siteUser);
         questionRepository.save(question);
+    }
+
+    public  Page<Question> getList(int page, String kw) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return questionRepository.findAllByKeyword(kw, pageable);
     }
 }
